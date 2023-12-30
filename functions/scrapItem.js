d8e71efee509f25668c1d843996bca67b80aeb50
itsparasbisht <iamparasbisht@gmail.com>
2023-12-30 18:37:58 +0530

    change in element selector method

--- functions/scrapItem.js
+++ functions/scrapItem.js
@@ -15,28 +15,40 @@ async function scrapItem(itemUrl) {
     const page = await browser.newPage();
     await page.goto(URL);
 
+    await page.waitForSelector("#productTitle");
+    let element = await page.$("#productTitle");
+    const title = await page.evaluate((el) => el.textContent, element);
+
+    await page.waitForSelector(".a-price-whole");
+    element = await page.$(".a-price-whole");
+    const price = await page.evaluate((el) => el.textContent, element);
+
+    await page.waitForSelector("#landingImage");
+    element = await page.$("#landingImage");
+    const image = await page.evaluate((el) => el.getAttribute("src"), element);
+
     // get item title
-    let title = await page.evaluate(() => {
-      let item = document.querySelector("#productTitle");
-      return item.innerText;
-    });
+    // let title = await page.evaluate(() => {
+    //   let item = document.querySelector("#productTitle");
+    //   return item.innerText;
+    // });
 
     // get item price
-    let price = await page.evaluate(() => {
-      let item = document.querySelector(".a-price-whole");
-      return parseInt(item.innerText.replace(/\D/g, ""));
-    });
+    // let price = await page.evaluate(() => {
+    //   let item = document.querySelector(".a-price-whole");
+    //   return parseInt(item.innerText.replace(/\D/g, ""));
+    // });
 
     // get item image
-    let image = await page.evaluate(() => {
-      let item = document.querySelector("#landingImage");
-      return item.getAttribute("src");
-    });
+    // let image = await page.evaluate(() => {
+    //   let item = document.querySelector("#landingImage");
+    //   return item.getAttribute("src");
+    // });
 
     return {
-      title,
+      title: title.trim(),
       image,
-      price,
+      price: parseInt(price.replace(/\D/g, "")),
     };
   } catch (error) {
     console.error("error - ", error);
